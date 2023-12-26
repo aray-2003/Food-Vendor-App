afaf65216040f3f0aafaf33d2ce77c8a1c651554
Avirup Ray <aray002@citymail.cuny.edu>
2023-12-26 12:58:03 -0500

    added linter and formatter

--- App.js
+++ App.js
@@ -1,27 +1,27 @@
-import FoodList  from './FoodList';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import FoodList from './FoodList'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, SafeAreaView } from 'react-native'
 
 const foodItems = [
-  { category: "Fruits", items: ["Apple", "Banana", "Orange"] },
-  { category: "Vegetables", items: ["Carrot", "Broccoli", "Spinach"] },
+  { category: 'Fruits', items: ['Apple', 'Banana', 'Orange'] },
+  { category: 'Vegetables', items: ['Carrot', 'Broccoli', 'Spinach'] }
   // Add more categories and items
-];
+]
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <FoodList foodItems={foodItems}/>
+      <FoodList foodItems={foodItems} />
       <StatusBar style="auto" />
     </SafeAreaView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 30,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    alignItems: 'center'
+  }
+})
--- FoodList.js
+++ FoodList.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import { View, Text, Pressable, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import React from 'react'
+import {
+  View,
+  Text,
+  Pressable,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native'
 
 // const foodItems = [
 //     { category: "Fruits", items: ["Apple", "Banana", "Orange"] },
@@ -8,51 +15,58 @@ import { View, Text, Pressable, FlatList, StyleSheet, TouchableOpacity } from 'r
 // ];
 
 const FoodList = ({ foodItems }) => {
-    const renderItem = ({ item }) => (
-        <View>
-            <Text style={styles.category}>{item.category}</Text>
-            {item.items.map(food => (
-                <TouchableOpacity style={styles.itemContainer} key={food} onPress={() => console.log(`Pressed ${food}`)}>
-                    <Text style={styles.item}>{food}</Text>
-                </TouchableOpacity>
-            ))}
-        </View>
-    );
+  const renderItem = ({ item }) => (
+    <View>
+      <Text style={styles.category}>{item.category}</Text>
+      {item.items.map((food) => (
+        <TouchableOpacity
+          style={styles.itemContainer}
+          key={food}
+          onPress={() => {
+            console.log(`Pressed ${food}`)
+          }}
+        >
+          <Text style={styles.item}>{food}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  )
 
-    return (
-        <FlatList 
-            data={foodItems} 
-            renderItem={renderItem} 
-            keyExtractor={item => item.category}
-        />
-    );
+  return (
+    <FlatList
+      data={foodItems}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.category}
+    />
+  )
 }
 
 const styles = StyleSheet.create({
-    category: {
-        fontSize: 18,
-        padding: 10,
-        backgroundColor: '#f0f0f0', // Light grey background for category
-    },
-    itemContainer: {
+  category: {
+    fontSize: 20,
+    padding: 10,
+    fontWeight: 'bold'
+  },
+  itemContainer: {
+    backgroundColor: '#3285f3', // Blue background for items
+    borderRadius: 5, // Rounded corners for items
+    padding: 10,
+    marginVertical: 5,
 
-        backgroundColor: '#D6E6F2', // Blue background for items
-        borderRadius: 5, // Rounded corners for items
-        padding: 10,
-        marginVertical: 5,
+    // shadow for ios
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
 
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.3,
-        shadowRadius: 3,
+    //shadow for android
+    elevation: Platform.OS === 'android' ? 3 : 0
+  },
+  item: {
+    color: '#FFFFFF', // White text color for better contrast
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
+})
 
-        elevation: Platform.OS === 'android' ? 3 : 0,
-    },
-    item: {
-        color: '#303841', // White text color for better contrast
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-});
-
-export default FoodList;
+export default FoodList
